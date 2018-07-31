78b522b96164f29d3b96c181bf7fd6555eb50b6e
carolflima <ncfl@ic.ufal.br>
2018-07-31 14:37:31 -0300

    resolve bug utf8

--- classif2.py
+++ classif2.py
@@ -3,6 +3,8 @@
     @author Natalia Lima
 """
 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
 from tpot import TPOTClassifier
 
@@ -25,10 +27,12 @@ def classificar():
     dataframe['DEST AUTOPECAS'] = [dest_autopecas[item] for item in dataframe['DEST AUTOPECAS']]
     dataframe.fillna(-1, inplace=True)
 
-    X_train, X_test, y_train, y_test = train_test_split(dataframe[LABELS].values, dataframe[TARGET].values, test_size=0.3)
+
+    X_train, X_test, y_train, y_test = train_test_split(dataframe[LABELS].values, dataframe.iloc[:, 0].values, test_size=0.3)
     tpot = TPOTClassifier(generations=5, population_size=50, verbosity=2)
     tpot.fit(X_train, y_train)
     print("%f" % tpot.score(X_test, y_test))
     tpot.export('tpot_classif_pipeline.py')
+
 
 classificar()
