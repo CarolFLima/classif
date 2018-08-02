55e66f235c06fe789a3c2b307a40055c975b0f99
carolflima <ncfl@ic.ufal.br>
2018-08-02 08:49:51 -0300

    Binariza o target

--- classif2.py
+++ classif2.py
@@ -6,9 +6,10 @@
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 from tpot import TPOTClassifier
-
 import pandas as pd
+import numpy as np
 
 LABELS = ['CEST PROD', 'DEST AUTOPECAS', 'COD_CST', 'COD CNAE EMIT', 'NCM PROD', 'TIP_FIN_NFE', 'COD CNAE DEST',
               'CFOP PROD']
@@ -19,19 +20,28 @@ def classificar():
     Extrai dataframe da tabela, converte variáveis de texto para número
     e preenche vazios para -1
     """
-    print("Comecei a extrair dataframe")
-    dataframe = pd.read_excel('data/data.xlsx')
-    print("Terminei")
 
+    dataframe = pd.read_excel('data/data.xlsx')
+
+    #dataframe.rename({'CODIFICAÇÃO': 'class'}, axis='columns', inplace=True)
+
+    # Binarizando variável com multiplos niveis
+    encoder = LabelEncoder()
+    classe_label = encoder.fit_transform(dataframe.iloc[:, 0])
+
+    print(classe_label)
+
+    # Binarizando variável com dois niveis
     dest_autopecas = {'N': 0, 'S': 1}
     dataframe['DEST AUTOPECAS'] = [dest_autopecas[item] for item in dataframe['DEST AUTOPECAS']]
+    # Preenchendo vazios com valor padrão
     dataframe.fillna(-1, inplace=True)
 
+    X_train, X_test, y_train, y_test = train_test_split(dataframe[LABELS].values, np.array(classe_label), test_size=0.3)
 
-    X_train, X_test, y_train, y_test = train_test_split(dataframe[LABELS].values, dataframe.iloc[:, 0].values, test_size=0.3)
-    tpot = TPOTClassifier(generations=5, population_size=50, verbosity=2)
+    tpot = TPOTClassifier(generations=5, population_size=50, verbosity=3)
     tpot.fit(X_train, y_train)
-    print("%f" % tpot.score(X_test, y_test))
+    print(tpot.score(X_test, y_test))
     tpot.export('tpot_classif_pipeline.py')
 
 
