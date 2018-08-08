0732ba6409ba7e5f9d3e5065705caec9bcac7162
carolflima <ncfl@ic.ufal.br>
2018-08-08 15:46:00 -0300

    add arquivo de teste

---
+++ teste.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Filtra e importa pra o csv
+dataframe = pd.read_excel('data/difal.xlsx')
+#
+dataframe['item.desc'] = dataframe['item.desc'].str.replace('-', ' ')
+dataframe['item.desc'] = dataframe['item.desc'].str.replace('[^ a-zA-Z]', '')
+dataframe['item.desc'] = dataframe['item.desc'].str.replace(' +', ' ')
+dataframe['item.desc'] = dataframe['item.desc'].str.lower()
+print(dataframe['item.desc'])
+dataframe.to_csv('data/difal.csv')
